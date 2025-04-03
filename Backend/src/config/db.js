21275f30281af0ef93aaf5452b020d26281978a7
Vo Quoc Quang <vqquangys25@gmail.com>
2025-04-03 23:09:19 +0700

    feat(database): config database to mongodb -> USE LOCAL

--- Backend/src/config/db.js
+++ Backend/src/config/db.js
@@ -1,23 +1,18 @@
-//lets require/import the mongodb native drivers.
-var mongodb = require('mongodb');
+const mongoose = require('mongoose');
 
-//We need to work with "MongoClient" interface in order to connect to a mongodb server.
-var MongoClient = mongodb.MongoClient;
+const connectDB = async () => {
+    try {
+        await mongoose.connect(process.env.DB_HOST, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+            serverSelectionTimeoutMS: 3000 // Thời gian chờ kết nối -> 30s
+        });
+        console.log(`Connected to mongodb at ${mongoose.connection.host}`);
 
-// Connection URL. This is where your mongodb server is running.
-var url = 'mongodb://localhost:8020/Bookshop';
-
-// Use connect method to connect to the Server
-MongoClient.connect(url, function (err, db) {
-    if (err) {
-        console.log('Unable to connect to the mongoDB server. Error:', err);
-    } else {
-        //HURRAY!! We are connected. :)
-        console.log('Connection established to', url);
-
-        // do some work here with the database.
-
-        //Close connection
-        db.close();
+    } catch (err) {
+        console.error("Connection error:", err.message);
+        process.exit(1);
     }
-});
+};
+
+module.exports = connectDB;
